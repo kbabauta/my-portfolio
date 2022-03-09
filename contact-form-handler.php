61a0511c61fa87071f1ae6054505c5022b29acc8
Kevin Babauta <kjr.babauta@gmail.com>
2022-03-08 20:22:11 -0800

    working contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,16 +1,6 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["subject"];
-	$content = $_POST["content"];
-
-	$toEmail = "admin@phppot_samples.com";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
+if($_POST)["Message"]) {
+    mail("kjr.babauta@gmail.com", "New Inquiry",
+    $_POST["Insert your Message"]. "From:")
 }
-require_once "contact-view.php";
 ?>
